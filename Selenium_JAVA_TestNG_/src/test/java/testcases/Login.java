1879b11567440904244441ae957b19f6b2d9c9c3
azmiblidaoui <azmi.blidaoui@gmail.com>
2023-05-05 19:05:54 +0100

    Login_with_valid_and_invalid_Credentilials

---
+++ Selenium_JAVA_TestNG_/src/test/java/testcases/Login.java
@@ -0,0 +1,48 @@
+package testcases;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Login {
+	@Test(priority = 1)
+	public void Login_with_valid_Credentilials()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.id("input-email")).sendKeys("tt5820724@gmail.com");
+		driver.findElement(By.id("input-password")).sendKeys("azerty@@123");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
+		driver.quit();
+		
+		
+	}
+	@Test(priority = 2)
+	public void Login_with_invalid_Credentilials()
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.id("input-email")).sendKeys("tt5820724ttt@gmail.com");
+		driver.findElement(By.id("input-password")).sendKeys("azerty1234");
+		driver.findElement(By.xpath("//input[@value='Login']")).click();
+		String actual_warning_message = driver.findElement(By.xpath("//div[@class='alert alert-danger alert-dismissible']")).getText();
+		String expected_warning_message = "Warning: No match for E-Mail Address and/or Password.";
+		Assert.assertTrue(actual_warning_message.contains(expected_warning_message),"Expected Warning Message is not displayed");
+		driver.quit();
+		
+		
+	}
+	
+
+}
